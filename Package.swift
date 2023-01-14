16c3f18505475736c390b90d254aea0c58249a71
Tyler Jones <tyler@tylerjones.io>
2023-01-14 12:18:37 -0700

    Bump release version

--- Package.swift
+++ Package.swift
@@ -3,21 +3,22 @@
 
 import PackageDescription
 
-let release = "v5.1"
+let release = "v5.1.1"
 
-let frameworks = ["ffmpegkit": "4c62d6a9d0f1661ae73497e7618fcd19b42671adb92fc1629d817b087568ea9b", "libavcodec": "9ba28f61bd25dddec796ad184a75bba1dc3db4163d652d42ab5df905cc004b5e", "libavdevice": "459f5d123dcbe498c064e1de60edef31c562aee96b9d2e987b1d15c7a76d25e4", "libavfilter": "f6ff7414f3d0e765ec9b4e36f7cf9e2334cada33490d1dfea1844c1ea8642999", "libavformat": "e1e7ced2c19cddae55a5c723816162a329af47c17111c3ee92c2d40e287765cd", "libavutil": "9fbfc78b4b570b85c9c900fa72e7882f4e702e8b20bb59cb058f8b2ea29e388f", "libswresample": "1850f5b9e4296c831326cad6b03bc44ffd4212e1ca89b124aaaeefaca86e2977", "libswscale": "11b7e863e8967cf48a0e1766cedc2afe68e7899c17c02043743f376922dc512d"]
+let frameworks = ["ffmpegkit": "69e1ebce712698722bd72e38f5cd6b97209b767c045958484af10c0d5f769532", "libavcodec": "2c3d3133fc57487382cc486bfd14f0552b2261447efcfe68ea648d80ecdd877c", "libavdevice": "f9a1d46029f4ff603325119686b445cea581b3e678682ac284d036967b9e6e24", "libavfilter": "985209d3ca5dec0dfb82d47d1b2cf49c35c17f6fe0def7ccda50b69d19b13dfc", "libavformat": "aaf8ad482f1887a5f08fe6fd8f24b2b02801571b0e96fbb7a7f18546f2d69880", "libavutil": "c1f04b391a874674a380e99464f11631ea62dd51fa9dcc0789c4cc3d56ab5198", "libswresample": "35ccf3b51dd35ddc70ae0a49049f192ecf273cc14c05ae849a3a07dffffec865", "libswscale": "b1af830516c5f99bad5f352ade94cbff748ff2515e4887495109dc6c044c4fdb"]
 
 func xcframework(_ package: Dictionary<String, String>.Element) -> Target {
-    let url = "https://github.com/tylerjonesio/ffmpeg-kit-spm/releases/download/\(release)/\(package.key).xcframework.zip"
-    return .binaryTarget(name: package.key, url: url, checksum: package.value)
+//    let url = "https://github.com/tylerjonesio/ffmpeg-kit-spm/releases/download/\(release)/\(package.key).xcframework.zip"
+//    return .binaryTarget(name: package.key, url: url, checksum: package.value)
+    return .binaryTarget(name: package.key, path: ".tmp/ffmpeg-kit/prebuilt/bundle-apple-xcframework/\(package.key).xcframework")
 }
 
 let linkerSettings: [LinkerSetting] = [
-    .linkedFramework("AudioToolbox"),
+    .linkedFramework("AudioToolbox", .when(platforms: [.macOS, .iOS, .macCatalyst, .tvOS])),
     .linkedFramework("AVFoundation", .when(platforms: [.macOS, .iOS, .macCatalyst])),
     .linkedFramework("CoreMedia", .when(platforms: [.macOS])),
     .linkedFramework("OpenGL", .when(platforms: [.macOS])),
-    .linkedFramework("VideoToolbox"),
+    .linkedFramework("VideoToolbox", .when(platforms: [.macOS, .iOS, .macCatalyst, .tvOS])),
     .linkedLibrary("z"),
     .linkedLibrary("lzma"),
     .linkedLibrary("bz2"),
@@ -28,7 +29,7 @@ let libAVFrameworks = frameworks.filter({ $0.key != "ffmpegkit" })
 
 let package = Package(
     name: "ffmpeg-kit-spm",
-    platforms: [.iOS(.v12), .macOS(.v10_15), .tvOS(.v11)],
+    platforms: [.iOS(.v12), .macOS(.v10_15), .tvOS(.v11), .watchOS(.v9)],
     products: [
         .library(
             name: "FFmpeg-Kit",
--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -e
 
-FFMPEG_KIT_TAG="v5.1"
-FFMPEG_KIT_CHECKOUT="origin/main"
+FFMPEG_KIT_TAG="v5.1.1"
+FFMPEG_KIT_CHECKOUT="origin/develop"
 #FFMPEG_KIT_CHECKOUT="origin/tags/$FFMPEG_KIT_TAG"
 
-FFMPEG_KIT_REPO="https://github.com/arthenica/ffmpeg-kit"
+FFMPEG_KIT_REPO="https://github.com/tylerjonesio/ffmpeg-kit"
 WORK_DIR=".tmp/ffmpeg-kit"
 
 if [[ ! -d $WORK_DIR ]]; then
@@ -18,6 +18,7 @@ fi
 
 echo "Checking out $FFMPEG_KIT_CHECKOUT..."
 cd $WORK_DIR
+git fetch
 git fetch --tags
 git checkout $FFMPEG_KIT_CHECKOUT
 
@@ -25,11 +26,14 @@ echo "Install build dependencies..."
 brew install autoconf automake libtool pkg-config curl git doxygen nasm bison wget gettext
 
 echo "Building for iOS..."
-./ios.sh --enable-ios-audiotoolbox --enable-ios-avfoundation --enable-ios-videotoolbox --enable-ios-zlib --enable-ios-bzip2 --no-bitcode --enable-gmp --enable-gnutls -x
+#./ios.sh --enable-ios-audiotoolbox --enable-ios-avfoundation --enable-ios-videotoolbox --enable-ios-zlib --enable-ios-bzip2 --no-bitcode --enable-gmp --enable-gnutls -x
 echo "Building for tvOS..."
-./tvos.sh --enable-tvos-audiotoolbox --enable-tvos-videotoolbox --enable-tvos-zlib --enable-tvos-bzip2 --no-bitcode --enable-gmp --enable-gnutls -x
+#./tvos.sh --enable-tvos-audiotoolbox --enable-tvos-videotoolbox --enable-tvos-zlib --enable-tvos-bzip2 --no-bitcode --enable-gmp --enable-gnutls -x
 echo "Building for macOS..."
-./macos.sh --enable-macos-audiotoolbox --enable-macos-avfoundation --enable-macos-bzip2 --enable-macos-videotoolbox --enable-macos-zlib --enable-macos-coreimage --enable-macos-opencl --enable-macos-opengl --enable-gmp --enable-gnutls -x
+#./macos.sh --enable-macos-audiotoolbox --enable-macos-avfoundation --enable-macos-bzip2 --enable-macos-videotoolbox --enable-macos-zlib --enable-macos-coreimage --enable-macos-opencl --enable-macos-opengl --enable-gmp --enable-gnutls -x
+echo "Building for watchOS..."
+./watchos.sh --enable-watchos-zlib --enable-watchos-bzip2 --no-bitcode --enable-gmp --enable-gnutls -x
+#./watchos.sh --enable-watchos-zlib --enable-watchos-bzip2 --no-bitcode -x
 
 echo "Bundling final XCFramework"
 ./apple.sh
