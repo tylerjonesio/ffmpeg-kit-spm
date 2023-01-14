be75afe45982a1228950f73cd8ae37842aae6cc0
Tyler Jones <tyler@tylerjones.io>
2023-01-14 00:37:39 -0700

    Fix package file

--- Package.swift
+++ Package.swift
@@ -8,9 +8,8 @@ let release = "v5.1"
 let frameworks = ["ffmpegkit": "4c62d6a9d0f1661ae73497e7618fcd19b42671adb92fc1629d817b087568ea9b", "libavcodec": "9ba28f61bd25dddec796ad184a75bba1dc3db4163d652d42ab5df905cc004b5e", "libavdevice": "459f5d123dcbe498c064e1de60edef31c562aee96b9d2e987b1d15c7a76d25e4", "libavfilter": "f6ff7414f3d0e765ec9b4e36f7cf9e2334cada33490d1dfea1844c1ea8642999", "libavformat": "e1e7ced2c19cddae55a5c723816162a329af47c17111c3ee92c2d40e287765cd", "libavutil": "9fbfc78b4b570b85c9c900fa72e7882f4e702e8b20bb59cb058f8b2ea29e388f", "libswresample": "1850f5b9e4296c831326cad6b03bc44ffd4212e1ca89b124aaaeefaca86e2977", "libswscale": "11b7e863e8967cf48a0e1766cedc2afe68e7899c17c02043743f376922dc512d"]
 
 func xcframework(_ package: Dictionary<String, String>.Element) -> Target {
-//    let url = "https://github.com/tylerjonesio/ffmpeg-kit-spm/releases/download/\(release)/\(package.key).xcframework.zip"
-//    return .binaryTarget(name: package.key, url: url, checksum: package.value)
-    return .binaryTarget(name: package.key, path: ".tmp/ffmpeg-kit/prebuilt/bundle-apple-xcframework/\(package.key).xcframework.zip")
+    let url = "https://github.com/tylerjonesio/ffmpeg-kit-spm/releases/download/\(release)/\(package.key).xcframework.zip"
+    return .binaryTarget(name: package.key, url: url, checksum: package.value)
 }
 
 let linkerSettings: [LinkerSetting] = [
